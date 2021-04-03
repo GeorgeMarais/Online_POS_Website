02feacb901d1614e5639cf8ebd1ec549e6a9bb4a
George Marais <maraisgeorge39@gmail.com>
2021-04-03 18:53:08 +0200

    Content Addition Code

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const menu = document.querySelector(".menu");
+const content = document.querySelector(".content");
 
-const content = `
+const menuContent = `
   <h1>Menu</h1>
-  <hr></br>
-  <button class="menu-btn">1</button>
-  <button class="menu-btn">2</button>
-  <button class="menu-btn">3</button>
-  <button class="menu-btn">4</button> 
+  <hr>
+  <button class="menu-btn" onclick="selectScreen()" value="order">Order</button>
+  <button class="menu-btn" onclick="selectScreen()" value="order">2</button>
+  <button class="menu-btn" onclick="selectScreen()" value="order">3</button>
+  <button class="menu-btn" onclick="selectScreen()" value="order">4</button> 
+  <div class="version"><p style="padding-left: 8%; padding-top: 1.5%;">Ver 1.0.0</p></div>
 `;
 
-menu.innerHTML = content;
+const orderContent = `
+  <head><h2>Order</h2></head>
+  <div class="item-table">
+    <table>
+      <tr>
+        <th>Item<th>
+        <th>Qty<th>
+        <th>Price<th>
+      </tr>
+    </table>
+  </div>
+  <div class="calc-content">
+`;
+
+menu.innerHTML = menuContent;
+
+function selectScreen(){
+  let screen = $("button").val();
+
+  switch (screen){
+    case "order":
+      content.innerHTML = orderContent;
+      break;
+    default:
+      console.log("Could not find screen");
+      break;
+  }
+}
