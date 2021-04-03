e1f9c0c2ae7c30178b10c6eabfbe657dfda4a005
George Marais <maraisgeorge39@gmail.com>
2021-04-03 18:05:16 +0200

    Components Added

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+
+const content = `
+  <h1>Menu</h1>
+  <hr></br>
+  <button class="menu-btn">1</button>
+  <button class="menu-btn">2</button>
+  <button class="menu-btn">3</button>
+  <button class="menu-btn">4</button> 
+`;
+
+menu.innerHTML = content;
